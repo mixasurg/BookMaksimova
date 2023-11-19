c34892c6a21f77330e45f886a0c81f2a7bac2cc3
Mixas <ya.mishanc@yandex.ru>
2023-11-19 14:51:25 +0300

    final

--- app/app.py
+++ app/app.py
@@ -4,98 +4,90 @@ from flask_login import LoginManager, UserMixin, login_user, login_required, log
 from sqlalchemy import Column, ForeignKey, Integer, Text, func
 from sqlalchemy.orm import relationship
 import time, random, string, os
+from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:mixasurg@localhost/Miniatures_store'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:mixasurg@localhost/Book_store_maksimova'
 app.config['SECRET_KEY'] = 'your_secret_key'
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
-# Модель Миниатюры
-class Miniature(db.Model):
-    __tablename__ = 'Miniatures'
-    miniature_ID = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.Text, nullable=False)
-    kategory_id = db.Column(db.Text, db.ForeignKey('Kategory.kategory_id'))
-    material_id = db.Column(db.Text, db.ForeignKey('Material.material_id'))
-    photos_id = db.Column(db.Integer, db.ForeignKey('Photos.photos_ID'))
-    artist_id = db.Column(db.Integer, db.ForeignKey('Users.user_id'))
+# Модель Продуктов
+class Products(db.Model):
+    __tablename__ = 'products'
+    product_id = db.Column(db.Integer, primary_key=True)
+    product_name = db.Column(db.Text, nullable=False)
+    price_purchase = db.Column(db.Integer, nullable=False)
+    price_sale = db.Column(db.Integer, nullable=False)
+    # product_category_code = db.Column(db.Integer, db.ForeignKey('Product_Categories.product_category_code'), nullable=False)
+    product_category_code = db.Column(db.Integer, db.ForeignKey('product_categories.product_category_code'), nullable=False)
+    cover = db.Column(db.Text)
 
     def __repr__(self):
-        return f"<Product {self.name}>"
+        return f"<Products {self.name}>"
     
 
 # Модель Пользователя
-class User(UserMixin, db.Model):
-    __tablename__ = 'Users'
-    user_id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.Text, nullable=False)
-    contact = db.Column(db.Text)
+class Client(UserMixin, db.Model):
+    __tablename__ = 'clients'
+    client_id = db.Column(db.Integer, primary_key=True)
+    client_name = db.Column(db.Text, nullable=False)
+    client_email = db.Column(db.Text)
     username = db.Column(db.Text, nullable = False)
     password = db.Column(db.Text, nullable = False)
-    is_artist = db.Column(db.Boolean, nullable = False)
-    is_active = db.Column(db.Boolean, default=True)  # Добавляем атрибут is_active
+    is_active = db.Column(db.Boolean, default=True) 
+    client_cart = [] 
     def get_id(self):
-        return str(self.user_id)
-
+        return str(self.client_id)
+    
+# Модель Работника
+class Worker(UserMixin, db.Model):
+    __tablename__ = 'workers'
+    worker_id = db.Column(db.Integer, primary_key=True)
+    worker_name = db.Column(db.Text, nullable=False)
+    username = db.Column(db.Text, nullable = False)
+    password = db.Column(db.Text, nullable = False)
+    is_active = db.Column(db.Boolean, default=True) 
+    is_worker = True  
+    client_cart = [] 
+    def get_id(self):
+        return str(self.worker_id)
 
 # Модель Заказы
 class Order(db.Model):
-    __tablename__ = 'Orders'
-    order_ID = db.Column(db.Integer, primary_key=True)
-    customer_ID = db.Column(db.Integer, db.ForeignKey('Users.user_id'))
-    status_id = db.Column(db.Integer, db.ForeignKey('Status.status_id'))
-    order_date = db.Column(db.Date)
-    completion_date = db.Column(db.Date)
-    description = db.Column(db.Text)
+    __tablename__ = 'orders'
+    order_id = db.Column(db.Integer, primary_key=True)
+    client_id = db.Column(db.Integer, db.ForeignKey('clients.client_id'))
+    worker_id = db.Column(db.Integer, db.ForeignKey('workers.worker_id'))
+    order_status_code = db.Column(db.Integer, db.ForeignKey('order_statuses.order_status_code'))
+    creation_data = db.Column(db.TIMESTAMP, nullable=False)
+    performance_data = db.Column(db.TIMESTAMP, nullable=False)
 
 # Модель Строки заказов
 class OrderLine(db.Model):
-    __tablename__ = 'Orders_row'
-    order_ID = db.Column(db.Integer, db.ForeignKey('Order.order_ID'), primary_key=True)
-    string_id = db.Column(db.Integer, primary_key=True)
-    artist_ID = db.Column(db.Integer, db.ForeignKey('Users.user_id'))
-    status_id = db.Column(db.Integer, db.ForeignKey('Status.status_id'))
-    order_date = db.Column(db.Date)
-    completion_date = db.Column(db.Date)
-    description = db.Column(db.Text)
-    amount = db.Column(db.Numeric)
-    product_id = db.Column(db.Integer,db.ForeignKey('Miniatures.miniature_ID'))
+    __tablename__ = 'order_strings'
+    order_id = db.Column(db.Integer, db.ForeignKey('Orders.order_id'), primary_key=True)
+    row_number = db.Column(db.Integer, primary_key=True)
+    product_amount = db.Column(db.Numeric)
+    product_id = db.Column(db.Integer,db.ForeignKey('Products.product_id'))
 
 # Модель Статусы
-class Status(db.Model):
-    __tablename__ = 'Status'
-    status_id = db.Column(db.Integer, primary_key=True)
-    status = db.Column(db.Text)
-
-# Модель Материалы
-class Material(db.Model):
-    __tablename__ = 'Material'
-    material_id = db.Column(db.Integer, primary_key=True)
-    material = db.Column(db.Text)
+class Order_statuses(db.Model):
+    __tablename__ = 'order_statuses'
+    order_status_code = db.Column(db.Integer, primary_key=True)
+    order_status = db.Column(db.Text)
 
 # Модель Категории
-class Kategory(db.Model):
-    __tablename__ = 'Kategory'
-    kategory_id = db.Column(db.Integer, primary_key=True)
-    kategory = db.Column(db.Text)
-
-# Модель Фото
-class Photo(db.Model):
-    __tablename__ = 'Photos'
-    photos_ID = db.Column(db.Integer, primary_key=True)
-    string_photo_id = db.Column(db.Integer, primary_key=True)
-    URL = db.Column(db.Text)
+class Product_Categories(db.Model):
+    __tablename__ = 'product_categories'
+    product_category_code = db.Column(db.Integer, primary_key=True)
+    product_category  = db.Column(db.Text)
 
 def get_last_order_id():
-    last_id = db.session.query(Order.order_ID).order_by(Order.order_ID.desc()).first()
+    last_id = db.session.query(Order.order_id).order_by(Order.order_id.desc()).first()
     last_order_id = last_id if last_id else 0
     return last_order_id
-
-def get_artist_id(miniature_id):
-    miniature = Miniature.query.filter_by(miniature_ID=miniature_id).first()
-    return miniature
 
 def generate_unique_filename():
     timestamp = str(int(time.time()))  # Get the current timestamp as a string
@@ -103,13 +95,8 @@ def generate_unique_filename():
     filename = timestamp + '_' + random_string + '.jpg'  # Combine the timestamp, random string, and file extension
     return filename
 
-def get_last_photos_id():
-    last_photo = db.session.query(Photo.photos_ID).order_by(Photo.photos_ID.desc()).first()
-    last_photos_id = last_photo[0] if last_photo else 0
-    return last_photos_id
-
 def save_photo(photo):
-    directory = 'static/photos'
+    directory = 'static/cover'
     if not os.path.exists(directory):
         os.makedirs(directory)
 
@@ -118,22 +105,21 @@ def save_photo(photo):
 
     photo.save(filepath)
 
-    photo_url = '/static/photos/' + filename
+    photo_url = '/static/cover/' + filename
     return photo_url
 
 @login_manager.user_loader
-def load_user(user_id):
-    return db.session.get(User, int(user_id))
+def load_user(client_id):
+    return db.session.get(Client, int(client_id))
+
+# s
 
 @app.route('/')
 def index():
-    products = Miniature.query.all()
+    products = Products.query.all()
     for product in products:
-        category = Kategory.query.get(product.kategory_id)
-        product.category_name = category.kategory
-        first_photo = Photo.query.filter_by(photos_ID=product.photos_id).first()
-        if first_photo:
-            product.first_photo_url = first_photo.URL
+        category = Product_Categories.query.get(product.product_category_code)
+        product.category_name = category.product_category
     return render_template('index.html', products=products)
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -143,9 +129,8 @@ def register():
         password = request.form['password']
         full_name = request.form['full_name']
         contact_info = request.form['contact_info']
-        is_artist = 'is_artist' in request.form
 
-        user = User(name=full_name, contact=contact_info, username=username, password=password, is_artist = is_artist)
+        user = Client(client_name=full_name, client_email=contact_info, username=username, password=password)
 
         db.session.add(user)
         db.session.commit()
@@ -159,13 +144,26 @@ def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        user = User.query.filter_by(username=username).first()
+        user = Client.query.filter_by(username=username).first()
         if user and user.password == password:
             login_user(user)
             return redirect(url_for('index'))
         else:
             return 'Invalid username or password'
     return render_template('login.html')
+
+@app.route('/login_worker', methods=['GET', 'POST'])
+def login_worker():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = Worker.query.filter_by(username=username).first()
+        if user and user.password == password:
+            login_user(user)
+            return redirect(url_for('index'))
+        else:
+            return 'Invalid username or password'
+    return render_template('login_worker.html')
 
 @app.route('/logout')
 @login_required
@@ -176,49 +174,62 @@ def logout():
 @app.route('/cart', methods=['GET', 'POST'])
 @login_required
 def cart():
+    product_ids = current_user.client_cart
+    products = Products.query.filter(Products.product_id.in_(product_ids)).all()
+    for product in products:
+        category = Product_Categories.query.get(product.product_category_code)
+        product.category_name = category.product_category
+        product.quantity = 1
+    return render_template('cart.html', products=products)
+
+@app.route('/add_cart', methods=['POST'])
+@login_required
+def add_cart():
     if request.method == 'POST':
         product_id = request.form['product_id']
+        current_user.client_cart.append(product_id)
+    return redirect(url_for('index'))
+
+@app.route('/check', methods=['POST'])
+@login_required
+def check():
+    if request.method == 'POST':
+        product_ids = current_user.client_cart
+        product_amount = int(request.form.get('productCount', 1)) 
         order_id = get_last_order_id() + 1
-        order = Order(order_ID = order_id, customer_ID=current_user.user_id)
+        order = Order(order_id=order_id, client_id=current_user.client_id, order_status_code=1, creation_data=datetime.now())
         db.session.add(order)
         db.session.commit()
-        artist_id = int(get_artist_id(product_id))
-        orderLine = OrderLine(order_ID = order_id, product_id = product_id, status_id = 1, artist_id = artist_id)
-        db.session.add(orderLine)
-        db.session.commit()
+        for product_id in product_ids:
+            order_line = OrderLine(order_id=order_id, product_id=product_id, product_amount=product_amount)
+            db.session.add(order_line)
+            db.session.commit()
         
         return redirect(url_for('cart'))
-    orders = Order.query.filter_by(user_id=current_user.id).all()
-    products = [Miniature.query.get(order.product_id) for order in orders]
-    return render_template('cart.html', products=products)
 
 @app.route('/add_product', methods=['GET', 'POST'])
 def add_product():
     if request.method == 'POST':
         name = request.form['name']
-        kategory_id = int(request.form.get('kategory'))
-        material_id = int(request.form.get('material'))
-        photos = request.files.getlist('photos[]')
-        photos_id = get_last_photos_id() + 1
-        user = current_user.user_id
-
+        product_category_code = int(request.form.get('kategory'))
+        price_purchase = int(request.form.get('pursh'))
+        price_sale = int(request.form.get('sale'))
+        
+        photos = request.files.getlist('cover[]')
+        photo_url = ''
         for photo in photos:
-        # Сохранение фотографии в хранилище и получение URL
             photo_url = save_photo(photo)
-            new_photo = Photo(photos_ID = photos_id, URL=photo_url)
-            db.session.add(new_photo)
-            db.session.commit()
-
-        new_product = Miniature(name=name, artist_id = user, kategory_id=kategory_id, material_id = material_id, photos_id = photos_id)
+        
+        print(photo_url)
+        new_product = Products(product_name=name, price_purchase =price_purchase, price_sale = price_sale, product_category_code=product_category_code, cover = photo_url)
 
         db.session.add(new_product)
         db.session.commit()
 
         return redirect(url_for('index'))
     else:
-        kategory = Kategory.query.all()
-        materials = Material.query.all()
-        return render_template('add_product.html', kategory=kategory, materials=materials)
+        kategory = Product_Categories.query.all()
+        return render_template('add_product.html', kategory=kategory)
     
 if __name__ == '__main__':
     app.run(debug=True)
